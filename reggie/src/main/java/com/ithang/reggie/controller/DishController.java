dd4f6fc6bfe23d3b64dd7580846fa3efb6311572
Yikoutian1 <599882460@qq.com>
2023-05-21 23:33:57 +0800

    add DishController

---
+++ reggie/src/main/java/com/ithang/reggie/controller/DishController.java
@@ -0,0 +1,36 @@
+package com.ithang.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ithang.reggie.common.R;
+import com.ithang.reggie.entity.Category;
+import com.ithang.reggie.entity.Dish;
+import com.ithang.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName DishController
+ * @Description TODO
+ * @Author QiuLiHang
+ * @DATE 2023/5/21 23:28
+ * @Version 1.0
+ */
+@Slf4j
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    public R<Page> page(int page, int pageSize) {
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+        // 构造条件构造器
+        LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+
+        return R.success(pageInfo);
+    }
+}
