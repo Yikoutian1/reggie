fd6b28d8bb7c7264ff24c9ac6f64f723e7ffcdc8
Yikoutian1 <599882460@qq.com>
2023-06-28 20:43:11 +0800

    update config

--- reggie/src/main/java/com/ithang/reggie/controller/OrderController.java
+++ reggie/src/main/java/com/ithang/reggie/controller/OrderController.java
@@ -1,14 +1,14 @@
 package com.ithang.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ithang.reggie.common.Result;
 import com.ithang.reggie.entity.Orders;
 import com.ithang.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @ClassName OrderController
@@ -35,4 +35,24 @@ public class OrderController {
         orderService.submit(orders);
         return Result.success("下单成功");
     }
+    /**
+     * 后台显示订单信息
+     */
+    @GetMapping("/page")
+    public Result<Page> page(int page, int pageSize, Long number, String beginTime, String endTime) {
+        log.info("page={},pageSize={},number={},beginTime={},endTime={}",page,pageSize,number,beginTime,endTime);
+        //分页构造器对象
+        Page<Orders> pageInfo = new Page<>(page,pageSize);
+        //构造条件查询对象
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //链式编程写查询条件
+        queryWrapper.like(number!=null,Orders::getNumber,number)
+                //前面加上判定条件是十分必要的，用户没有填写该数据，查询条件上就不添加它
+                .gt(StringUtils.isNotBlank(beginTime),Orders::getOrderTime,beginTime)//大于起始时间
+                .lt(StringUtils.isNotBlank(endTime),Orders::getOrderTime,endTime);//小于结束时间
+        orderService.page(pageInfo,queryWrapper);
+        return Result.success(pageInfo);
+    }
+
+
 }
